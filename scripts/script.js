4dfc82a5b7b828ed544898308eb1dc4adb67610f
Wiston Uribe Marin <wiston.uribe@correounivalle.edu.co>
2022-09-15 12:37:16 +0200

    Avance de ejercicios de javaScript modulo II

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,47 @@
-alert("hello!");
+/*
+ Ejercicio I:
+ Abrir y ejecutar los archivos de la carpeta JavaScript-Module-II
+*/
 
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
+/*
+ Ejercicio II:
+ let element= document.querySelector("#create");
+ console.log(element);
+*/
+
+/* 
+ Ejercicio III:
+ let element = document.querySelector("#create");
+ element.style.backgroundColor = "green";
+*/
+
+/* 
+ Ejercicio IV:
+ document.querySelector('#new-todo').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const text = e.target.elements.text.value.trim()
+    console.log(text)
+ })
+ */
+
+/* 
+ Ejercicio V:
+
+ let todos=[];
+
+ createTodos=(text)=>{
+   todos.push(text);
+ }
+
+ document.querySelector('#new-todo').addEventListener('submit', (e) => {
     e.preventDefault();
     const text = e.target.elements.text.value.trim();
-    alert(text);
-})
+
+    if(text.length>0){
+      createTodos(text);
+      e.target.elements.text.value = ''
+    }
+ 
+    console.log(todos);
+ })
+ */
