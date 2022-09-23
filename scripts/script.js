261b065baa575fb9e365d7b778d5b9712943d7a4
Wiston Uribe Marin <wiston.uribe@correounivalle.edu.co>
2022-09-23 22:46:35 +0200

    Ejemplos de clase javaScript II

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,734 @@
+/* *** Ejercicio 17: ***/
+let todos = [];
+
+const renderTodos = (todos) => {
+    let filteredTodos = todos.filter((todo) => todo.title.toLowerCase().includes(filters.searchTitle.toLowerCase()))
+    if(filters.showFinished && filters.showUnfinished) {
+
+    } else if(filters.showFinished) {
+      filteredTodos = filteredTodos.filter((todo) => todo.completed)
+    } else if(filters.showUnfinished) {
+      filteredTodos = filteredTodos.filter((todo) => !todo.completed)
+    }
+    
+    const todoList = document.querySelector('#todos')
+    todoList.innerHTML = ''
+
+    if (filteredTodos.length > 0) {
+        filteredTodos.forEach((todo) => {
+            todoList.appendChild(generateTodoDOM(todo))
+        })
+    } else {
+        const messageEl = document.createElement('p')
+        messageEl.classList.add('empty-message')
+        messageEl.textContent = 'There are no todos to show'
+        todoList.appendChild(messageEl)
+    }
+}
+
+const setFilters = (updates) => {
+  if (typeof updates.searchTitle === "string") {
+    filters.searchTitle = updates.searchTitle;
+  }
+  if (typeof updates.showFinished === "boolean") {
+    filters.showFinished = updates.showFinished;
+  }
+  if (typeof updates.showUnfinished === "boolean") {
+    filters.showUnfinished = updates.showUnfinished;
+  }
+};
+
+const filters = {
+  searchTitle: "",
+  showFinished: false,
+  showUnfinished: false,
+};
+
+const toggleTodo = (title) => {
+  const todo = todos.find(
+    (todo) => todo.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (todo) {
+    todo.completed = !todo.completed;
+  }
+};
+
+const removeTodo = (title) => {
+  const todoIndex = todos.findIndex(
+    (todo) => todo.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (todoIndex > -1) {
+    todos.splice(todoIndex, 1);
+  }
+};
+
+const generateTodoDOM = (todoObj) => {
+  const todoEl = document.createElement("label");
+  const containerEl = document.createElement("div");
+  const todoText = document.createElement("span");
+
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.checked = todoObj.completed;
+  containerEl.appendChild(checkbox);
+  checkbox.addEventListener("change", () => {
+    toggleTodo(todoObj.title);
+    renderTodos(todos);
+  });
+
+  todoText.textContent = todoObj.title;
+  containerEl.appendChild(todoText);
+
+  todoEl.classList.add("list-item");
+  containerEl.classList.add("list-item__container");
+  todoEl.appendChild(containerEl);
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "remove";
+  removeButton.classList.add("button", "button--text");
+  todoEl.appendChild(removeButton);
+  removeButton.addEventListener("click", () => {
+    removeTodo(todoObj.title);
+    renderTodos(todos);
+  });
+
+  return todoEl;
+};
+
+const createTodo = (text) => {
+  todos.push({
+    title: text,
+    completed: false,
+  });
+};
+
+document.querySelector("#search-text").addEventListener("input", (e) => {
+  setFilters({
+    searchTitle: e.target.value,
+  });
+  renderTodos(todos);
+});
+
+document.querySelector("#show-finished").addEventListener("change", (e) => {
+  setFilters({
+    showFinished: e.target.checked,
+  });
+  renderTodos(todos);
+});
+
+document.querySelector("#show-unfinished").addEventListener("change", (e) => {
+  setFilters({
+    showUnfinished: e.target.checked,
+  });
+  renderTodos(todos);
+});
+
+document.querySelector("#new-todo").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const text = e.target.elements.text.value.trim();
+
+  if (text.length > 0) {
+    createTodo(text);
+    e.target.elements.text.value = "";
+  }
+
+  renderTodos(todos);
+});
+
+/* *** Ejercicio 16: ***/
+/*
+let todos = [];
+
+const renderTodos = (todos) => {
+  const todoList = document.querySelector("#todos");
+  todoList.innerHTML = "";
+
+  if (todos.length > 0) {
+    todos.forEach((todo) => {
+      todoList.appendChild(generateTodoDOM(todo));
+    });
+  } else {
+    const messageEl = document.createElement("p");
+    messageEl.classList.add("empty-message");
+    messageEl.textContent = "There are no todos to show";
+    todoList.appendChild(messageEl);
+  }
+};
+
+const setFilters = (updates) => {
+  if (typeof updates.searchTitle === "string") {
+    filters.searchTitle = updates.searchTitle;
+  }
+  if (typeof updates.showFinished === "boolean") {
+    filters.showFinished = updates.showFinished;
+  }
+  if (typeof updates.showUnfinished === "boolean") {
+    filters.showUnfinished = updates.showUnfinished;
+  }
+};
+
+const filters = {
+  searchTitle: "",
+  showFinished: false,
+  showUnfinished: false,
+};
+
+const toggleTodo = (title) => {
+  const todo = todos.find(
+    (todo) => todo.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (todo) {
+    todo.completed = !todo.completed;
+  }
+};
+
+const removeTodo = (title) => {
+  const todoIndex = todos.findIndex(
+    (todo) => todo.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (todoIndex > -1) {
+    todos.splice(todoIndex, 1);
+  }
+};
+
+const generateTodoDOM = (todoObj) => {
+  const todoEl = document.createElement("label");
+  const containerEl = document.createElement("div");
+  const todoText = document.createElement("span");
+
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.checked = todoObj.completed;
+  containerEl.appendChild(checkbox);
+  checkbox.addEventListener("change", () => {
+    toggleTodo(todoObj.title);
+    renderTodos(todos);
+  });
+
+  todoText.textContent = todoObj.title;
+  containerEl.appendChild(todoText);
+
+  todoEl.classList.add("list-item");
+  containerEl.classList.add("list-item__container");
+  todoEl.appendChild(containerEl);
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "remove";
+  removeButton.classList.add("button", "button--text");
+  todoEl.appendChild(removeButton);
+  removeButton.addEventListener("click", () => {
+    removeTodo(todoObj.title);
+    renderTodos(todos);
+  });
+
+  return todoEl;
+};
+
+const createTodo = (text) => {
+  todos.push({
+    title: text,
+    completed: false,
+  });
+};
+
+document.querySelector("#search-text").addEventListener("input", (e) => {
+  setFilters({
+    searchTitle: e.target.value,
+  });
+  renderTodos(todos);
+});
+
+document.querySelector("#show-finished").addEventListener("change", (e) => {
+  setFilters({
+    showFinished: e.target.checked,
+  });
+  renderTodos(todos);
+});
+
+document.querySelector("#show-unfinished").addEventListener("change", (e) => {
+  setFilters({
+    showUnfinished: e.target.checked,
+  });
+  renderTodos(todos);
+});
+
+document.querySelector("#new-todo").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const text = e.target.elements.text.value.trim();
+
+  if (text.length > 0) {
+    createTodo(text);
+    e.target.elements.text.value = "";
+  }
+
+  renderTodos(todos);
+});
+*/
+/* *** Ejercicio 15: ***/
+/*
+let todos = [];
+
+const renderTodos = (todos) => {
+  const todoList = document.querySelector("#todos");
+  todoList.innerHTML = "";
+
+  if (todos.length > 0) {
+    todos.forEach((todo) => {
+      todoList.appendChild(generateTodoDOM(todo));
+    });
+  } else {
+    const messageEl = document.createElement("p");
+    messageEl.classList.add("empty-message");
+    messageEl.textContent = "There are no todos to show";
+    todoList.appendChild(messageEl);
+  }
+};
+
+const setFilters = (updates) => {
+    if (typeof updates.searchTitle === 'string') {
+        filters.searchTitle = updates.searchTitle
+    }
+    if (typeof updates.showFinished === 'boolean') {
+        filters.showFinished = updates.showFinished
+    }
+    if (typeof updates.showUnfinished === 'boolean') {
+        filters.showUnfinished = updates.showUnfinished
+    }
+}
+
+const filters = {
+    searchTitle: '',
+    showFinished: false,
+    showUnfinished: false
+}
+
+const toggleTodo = (title) => {
+  const todo = todos.find(
+    (todo) => todo.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (todo) {
+    todo.completed = !todo.completed;
+  }
+};
+
+const removeTodo = (title) => {
+  const todoIndex = todos.findIndex(
+    (todo) => todo.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (todoIndex > -1) {
+    todos.splice(todoIndex, 1);
+  }
+};
+
+const generateTodoDOM = (todoObj) => {
+  const todoEl = document.createElement("label");
+  const containerEl = document.createElement("div");
+  const todoText = document.createElement("span");
+
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.checked = todoObj.completed;
+  containerEl.appendChild(checkbox);
+  checkbox.addEventListener("change", () => {
+    toggleTodo(todoObj.title);
+    renderTodos(todos);
+  });
+
+  todoText.textContent = todoObj.title;
+  containerEl.appendChild(todoText);
+
+  todoEl.classList.add("list-item");
+  containerEl.classList.add("list-item__container");
+  todoEl.appendChild(containerEl);
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "remove";
+  removeButton.classList.add("button", "button--text");
+  todoEl.appendChild(removeButton);
+  removeButton.addEventListener("click", () => {
+    removeTodo(todoObj.title);
+    renderTodos(todos);
+  });
+
+  return todoEl;
+};
+
+const createTodo = (text) => {
+  todos.push({
+    title: text,
+    completed: false,
+  });
+};
+
+document.querySelector("#new-todo").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const text = e.target.elements.text.value.trim();
+
+  if (text.length > 0) {
+    createTodo(text);
+    e.target.elements.text.value = "";
+  }
+
+  renderTodos(todos);
+});
+*/
+/* *** Ejercicio 14: ***/
+/*
+let todos = [];
+
+const renderTodos = (todos) => {
+  const todoList = document.querySelector("#todos");
+  todoList.innerHTML = "";
+
+  if (todos.length > 0) {
+    todos.forEach((todo) => {
+      todoList.appendChild(generateTodoDOM(todo));
+    });
+  } else {
+    const messageEl = document.createElement("p");
+    messageEl.classList.add("empty-message");
+    messageEl.textContent = "There are no todos to show";
+    todoList.appendChild(messageEl);
+  }
+};
+
+const filters = {
+    searchTitle: '',
+    showFinished: false,
+    showUnfinished: false
+}
+
+const toggleTodo = (title) => {
+  const todo = todos.find(
+    (todo) => todo.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (todo) {
+    todo.completed = !todo.completed;
+  }
+};
+
+const removeTodo = (title) => {
+  const todoIndex = todos.findIndex(
+    (todo) => todo.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (todoIndex > -1) {
+    todos.splice(todoIndex, 1);
+  }
+};
+
+const generateTodoDOM = (todoObj) => {
+  const todoEl = document.createElement("label");
+  const containerEl = document.createElement("div");
+  const todoText = document.createElement("span");
+
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.checked = todoObj.completed;
+  containerEl.appendChild(checkbox);
+  checkbox.addEventListener("change", () => {
+    toggleTodo(todoObj.title);
+    renderTodos(todos);
+  });
+
+  todoText.textContent = todoObj.title;
+  containerEl.appendChild(todoText);
+
+  todoEl.classList.add("list-item");
+  containerEl.classList.add("list-item__container");
+  todoEl.appendChild(containerEl);
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "remove";
+  removeButton.classList.add("button", "button--text");
+  todoEl.appendChild(removeButton);
+  removeButton.addEventListener("click", () => {
+    removeTodo(todoObj.title);
+    renderTodos(todos);
+  });
+
+  return todoEl;
+};
+
+const createTodo = (text) => {
+  todos.push({
+    title: text,
+    completed: false,
+  });
+};
+
+document.querySelector("#new-todo").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const text = e.target.elements.text.value.trim();
+
+  if (text.length > 0) {
+    createTodo(text);
+    e.target.elements.text.value = "";
+  }
+
+  renderTodos(todos);
+});
+*/
+
+/* *** Ejercicio 13: ***/
+/*
+let todos = [];
+
+const renderTodos = (todos) => {
+  const todoList = document.querySelector("#todos");
+  todoList.innerHTML = "";
+
+  if (todos.length > 0) {
+    todos.forEach((todo) => {
+      todoList.appendChild(generateTodoDOM(todo));
+    });
+  } else {
+    const messageEl = document.createElement("p");
+    messageEl.classList.add("empty-message");
+    messageEl.textContent = "There are no todos to show";
+    todoList.appendChild(messageEl);
+  }
+};
+
+const toggleTodo = (title) => {
+  const todo = todos.find(
+    (todo) => todo.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (todo) {
+    todo.completed = !todo.completed;
+  }
+};
+
+const removeTodo = (title) => {
+  const todoIndex = todos.findIndex(
+    (todo) => todo.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (todoIndex > -1) {
+    todos.splice(todoIndex, 1);
+  }
+};
+
+const generateTodoDOM = (todoObj) => {
+  const todoEl = document.createElement("label");
+  const containerEl = document.createElement("div");
+  const todoText = document.createElement("span");
+
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.checked = todoObj.completed;
+  containerEl.appendChild(checkbox);
+  checkbox.addEventListener("change", () => {
+    toggleTodo(todoObj.title);
+    renderTodos(todos);
+  });
+
+  todoText.textContent = todoObj.title;
+  containerEl.appendChild(todoText);
+
+  todoEl.classList.add("list-item");
+  containerEl.classList.add("list-item__container");
+  todoEl.appendChild(containerEl);
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "remove";
+  removeButton.classList.add("button", "button--text");
+  todoEl.appendChild(removeButton);
+  removeButton.addEventListener("click", () => {
+    removeTodo(todoObj.title);
+    renderTodos(todos);
+  });
+
+  return todoEl;
+};
+
+const createTodo = (text) => {
+  todos.push({
+    title: text,
+    completed: false,
+  });
+};
+
+document.querySelector("#new-todo").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const text = e.target.elements.text.value.trim();
+
+  if (text.length > 0) {
+    createTodo(text);
+    e.target.elements.text.value = "";
+  }
+
+  renderTodos(todos);
+});
+*/
+
+/* *** Ejercicio 12: ***/
+/*
+let todos = [];
+
+const renderTodos = (todos) => {
+  const todoList = document.querySelector("#todos");
+  todoList.innerHTML = "";
+
+  if (todos.length > 0) {
+    todos.forEach((todo) => {
+      todoList.appendChild(generateTodoDOM(todo));
+    });
+  } else {
+    const messageEl = document.createElement("p");
+    messageEl.classList.add("empty-message");
+    messageEl.textContent = "There are no todos to show";
+    todoList.appendChild(messageEl);
+  }
+};
+
+const removeTodo = (todoEl) => {
+  const todoIndex = todos.findIndex((todo) => {
+    return todo.toLowerCase() === todoEl.textContent.toLowerCase();
+  });
+  if (todoIndex > -1) {
+    todos.splice(todoIndex, 1);
+  }
+};
+
+const generateTodoDOM = (todoObj) => {
+    const todoEl = document.createElement('label')
+    const containerEl = document.createElement('div')
+    const todoText = document.createElement('span')
+
+    const checkbox = document.createElement('input')
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox.checked = todoObj.completed
+    containerEl.appendChild(checkbox)
+    checkbox.addEventListener('change', () => {
+        toggleTodo(todoObj.title)
+        renderTodos(todos)
+    })
+
+    todoText.textContent = todoObj.title
+    containerEl.appendChild(todoText)
+
+    todoEl.classList.add('list-item')
+    containerEl.classList.add('list-item__container')
+    todoEl.appendChild(containerEl)
+
+    const removeButton = document.createElement('button')
+    removeButton.textContent = 'remove'
+    removeButton.classList.add('button', 'button--text')
+    todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', () => {
+        removeTodo(todoObj.title)
+        renderTodos(todos)
+    })
+
+    return todoEl
+}
+
+const createTodo = (text) => {
+    todos.push({
+        title: text,
+        completed: false
+    })
+}
+
+document.querySelector("#new-todo").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const text = e.target.elements.text.value.trim();
+
+  if (text.length > 0) {
+    createTodo(text);
+    e.target.elements.text.value = "";
+  }
+ 
+  renderTodos(todos);
+});
+*/
+
+/* *** Ejercicio 11: ***/
+/*
+let todos = [];
+
+const renderTodos = (todos) => {
+  const todoList = document.querySelector("#todos");
+  todoList.innerHTML = "";
+
+  if (todos.length > 0) {
+    todos.forEach((todo) => {
+      todoList.appendChild(generateTodoDOM(todo));
+    });
+  } else {
+    const messageEl = document.createElement("p");
+    messageEl.classList.add("empty-message");
+    messageEl.textContent = "There are no todos to show";
+    todoList.appendChild(messageEl);
+  }
+};
+
+const removeTodo = (todoEl) => {
+  const todoIndex = todos.findIndex((todo) => {
+    return todo.toLowerCase() === todoEl.textContent.toLowerCase();
+  });
+  if (todoIndex > -1) {
+    todos.splice(todoIndex, 1);
+  }
+};
+
+const generateTodoDOM = (todo) => {
+  const todoEl = document.createElement("label");
+  todoEl.classList.add("list-item");
+
+  const containerEl = document.createElement("div");
+  containerEl.classList.add("list-item__container");
+
+  const todoText = document.createElement("span");
+
+  todoText.textContent = todo;
+  containerEl.appendChild(todoText);
+  todoEl.appendChild(containerEl);
+
+  const removeButton = document.createElement("button");
+  removeButton.classList.add("button", "button--text");
+  removeButton.textContent = "remove";
+  todoEl.appendChild(removeButton);
+
+  removeButton.addEventListener("click", () => {
+    removeTodo(todoText);
+    renderTodos(todos);
+  });
+    
+  return todoEl;
+};
+
+const createTodo = (text) => {
+    todos.push({
+        title: text,
+        completed: false
+    })
+}
+
+document.querySelector("#new-todo").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const text = e.target.elements.text.value.trim();
+
+  if (text.length > 0) {
+    createTodo(text);
+    e.target.elements.text.value = "";
+  }
+ 
+  renderTodos(todos);
+});
+*/
+
 /* *** Ejercicio 10: ***/
+/*
 let todos = [];
 
 const renderTodos = (todos) => {
@@ -64,6 +794,7 @@ document.querySelector("#new-todo").addEventListener("submit", (e) => {
   }
   renderTodos(todos);
 });
+*/
 
 /* *** Ejercicio 9: *** 
 let todos = [];
